6960741ed7d4149821c92c65175ce05934c21456
katerinanikou <katerina.nikou@sunnyatsea.se>
2018-04-10 19:09:23 +0200

    api structure

---
+++ api.php
@@ -0,0 +1,25 @@
+<?php 
+
+	//skicka statuskoder för underlätta om du får fel
+	//500 server error
+	//200 ok
+	//400 bad request
+	switch ($_SERVER["REQUEST_METHOD"]) {
+		case 'GET':
+			//hantera get anrop
+			//get brukar vara hämta något från databasen och skicka tillbaka det.
+			//glöm inte att skicka tillbaka data med json
+			break;
+		case 'POST':
+			//hantera post anrop
+			//post brukar vara spara något
+			break;
+		case 'DELETE':
+			//hantera delete anrop
+			//delete är ta bort
+			break;
+		case 'PUT':
+			//hantera put anrop
+			//uppdatera data
+			break;
+	}
